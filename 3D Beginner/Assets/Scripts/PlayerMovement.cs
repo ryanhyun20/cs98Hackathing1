e19faa385bc300c40f55b7903f2641689ef12d58
ryanhyun20 <ryan0304hyun@gmail.com>
2020-01-12 13:50:03 -0800

    enemy implementation, audio, tidying up files and environment

--- 3D Beginner/Assets/Scripts/PlayerMovement.cs
+++ 3D Beginner/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     // reference to Animator component
     Animator m_Animator;
     Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
 
     // variable that represents change in position
     Vector3 m_Movement;
@@ -21,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
     {
         m_Animator = GetComponent<Animator> ();
         m_Rigidbody = GetComponent<Rigidbody> ();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -40,6 +42,17 @@ public class PlayerMovement : MonoBehaviour
       bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
       bool isWalking = hasHorizontalInput || hasVerticalInput;
       m_Animator.SetBool("IsWalking", isWalking);
+      if(isWalking)
+      {
+        if(!m_AudioSource.isPlaying)
+        {
+          m_AudioSource.Play ();
+        }
+      }
+      else
+      {
+        m_AudioSource.Stop ();
+      }
 
       // forward vector calculation
       Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
